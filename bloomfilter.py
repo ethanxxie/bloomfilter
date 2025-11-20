c22ed8ecc6cd376b6de273f8664464f728cbd328
Ethan Xie <ethanxie@Mac-Studio.local>
2025-11-19 23:47:22 -0800

    cleaned up comments

--- bloomfilter.py
+++ bloomfilter.py
@@ -1,4 +1,3 @@
-import hashlib
 from bitarray import bitarray
 import math
 import random
@@ -67,52 +66,45 @@ def hash_table(data, hash_type, m):
 # ------------------------------------------------------------------------------------------------------
 
 
-
-# Define four datasets (data types)
+'''
+# define two kinds of datasets (data types)
 random_data = [rng.randrange(N) for _ in range(5 * m_1)]                   # random data from 0 to N
 correlated_data = [2*n for n in range(5 * m_1)]                            # 0, 2, 4, ..., 20*m
-# even_data = [2*n for n in range(5 * m_1)]                                  # 0, 2, 4, ..., 20*m
-# value_pool = [rng.randrange(N) for _ in range(5)]                          # 5 unique values to be repeated in repeated_data
-# repeated_data = [random.choice(value_pool) for _ in range(5 * m_1)]        # repeated values
 
 
-def plot_buckets_on_plotsis(buckets, plot, title):
+def plot_buckets_on_plotsis(buckets, plot, title):                         # plots the buckets on given plot    
     plot.bar(range(len(buckets)), buckets)
     plot.set_xlabel("Bucket index")
     plot.set_ylabel("Count")
     plot.set_title(title)
 
 
-# Prepare datasets list for iteration
+# prepare datasets list for iteration
 datasets = [
     ("Random Data", random_data),
     ("Correlated Data", correlated_data)
-    # ("Even Data", even_data),
-    # ("Repeated Data", repeated_data)
 ]
 
-# Compute hash tables for each dataset with hash1 and hash2
+# compute hash tables for each dataset with hash1 and hash2
 hash1_buckets_list = [hash_table(data, 1, m_1) for (_name, data) in datasets]
 hash2_buckets_list = [hash_table(data, 2, m_1) for (_name, data) in datasets]
 
-# Plot Hash1 results: one figure with 2 subplots (1x2)
-fig1, plotses1 = plt.subplots(1, 2, figsize=(12, 8))
-plotses1 = plotses1.ravel()
+fig1, plotses1 = plt.subplots(1, 2, figsize=(12, 8))                       # create subplots for hash1 results       
+plotses1 = plotses1.ravel()                                                # flattens the array of plots for easy looping
 
-for i, (name, _data) in enumerate(datasets):
+for i, (name, _data) in enumerate(datasets):                               # plot each dataset
     plot_buckets_on_plotsis(hash1_buckets_list[i], plotses1[i], f"Hash1 - {name}")
 plt.tight_layout()
 plt.show()
 
-# Plot Hash2 results: one figure with 2 subplots (1x2)
-fig2, plotses2 = plt.subplots(1, 2, figsize=(12, 8))
+fig2, plotses2 = plt.subplots(1, 2, figsize=(12, 8))                       # repeat for hash2 results
 plotses2 = plotses2.ravel()
 
 for i, (name, _data) in enumerate(datasets):
     plot_buckets_on_plotsis(hash2_buckets_list[i], plotses2[i], f"Hash2 - {name}")
 plt.tight_layout()
 plt.show()
-
+'''
 
 
 # ------------------------------------------------------------------------------------------------------
@@ -141,7 +133,7 @@ class BloomFilter:
         if (self.hash_type == 1):                                      
             bits = hash1(x, self.m, self.k, self.a, self.b)     # find the hash1 position
         else:       
-            bits = hash2(x, self.m, self.seeds)         # find the hash2 position
+            bits = hash2(x, self.m, self.seeds)                 # find the hash2 position
 
         for bit in bits:        
             self.bitarray[bit] = 1                              # cover the bits for both hash positions
@@ -150,42 +142,42 @@ class BloomFilter:
         if (self.hash_type == 1):                                            
             bits = hash1(x, self.m, self.k, self.a, self.b)     # find the hash1 position
         else:       
-            bits = hash2(x, self.m, self.seeds)         # find the hash2 position
+            bits = hash2(x, self.m, self.seeds)                 # find the hash2 position
 
         # checks if every bit of x in the bitarray is set to 1 (meaning its in the maintained set)
         return all(self.bitarray[bit] == 1 for bit in bits)
 
 
-# calculates false positive rate for a given size
+# calculates false positive rate for a given subset size n, universe size N, c value, k value, and hash type
 def false_positve_rate(n, N, c, k, hash_type):
-    bf = BloomFilter(n, c, k, hash_type)                    # create bloom filter
-    m = c * n                                               # hash table size
+    bf = BloomFilter(n, c, k, hash_type)                        # create bloom filter
+    m = c * n                                                   # hash table size
 
-    universe_sample = random.sample(range(N), n)            # universe of size N = |U|
-    inserted_set = set(universe_sample)                     # used to exclude true positives in test
+    universe_sample = random.sample(range(N), n)                # universe of size N = |U|
+    inserted_set = set(universe_sample)                         # used to exclude true positives in test
 
-    for x in universe_sample:                               # insert elements
-        bf.add(x)
+    for x in universe_sample:                                   # insert elements
+        bf.add(x)   
 
-    false_pos = 0                                           # track number of false positives
-    tests_done = 0                                          # used to find false positives rate
+    false_pos = 0                                               # track number of false positives
+    tests_done = 0                                              # used to find false positives rate
 
-    while tests_done < n:                                   # runs n tests
-        x = random.randrange(N)                             # pick a random sample from N
-        if x in inserted_set:                      
-            continue                                        # we don't care about true positives, only false
-        tests_done += 1                    
-        if bf.contains(x):                                  # if the bloom filter says it's there even 
-            false_pos += 1                     
+    while tests_done < n:                                       # runs n tests
+        x = random.randrange(N)                                 # pick a random sample from N
+        if x in inserted_set:                       
+            continue                                            # we don't care about true positives, only false
+        tests_done += 1                     
+        if bf.contains(x):                                      # if the bloom filter says it's there even 
+            false_pos += 1                      
 
-    return false_pos / tests_done                           # ratio of false positives tests to total tests
+    return false_pos / tests_done                               # ratio of false positives tests to total tests
 
 
-# Plots the median value from 10 test runs of each k-value for a given c value
+# plots median value from 10 test runs for of each k-value for a given c value
 def plot_fpr(n, N, c, k_values, tests, plot):
-    m = c * n                                               # size of hash table
-    hash1_fprs = []                                         # array of false positive rates for each c value
-    hash2_fprs = []                                         # array of false positive rates for each c value
+    m = c * n                                                   # size of hash table
+    hash1_fprs = []                                             # array of false positive rates for each c value
+    hash2_fprs = []                                             # array of false positive rates for each c value
 
     for k in range(k_values):                               
         hash1_test_results = []                                             # store each test run result from hash1 and hash2 separately
@@ -199,47 +191,45 @@ def plot_fpr(n, N, c, k_values, tests, plot):
         hash2_fprs.append(hash2_test_median)                               
 
 
-    ideal_rate = [(1 - math.exp(-k/c))**k for k in range(1, k_values + 1)]
-    
-    # plotting figure
-    plot.plot(range(2, k_values + 1), hash1_fprs[1:], marker="x", label=f"c={c}, hash1")        # plots hash1 simulated fpr
-    plot.plot(range(2, k_values + 1), hash2_fprs[1:], marker="x", label=f"c={c}, hash2")        # plots hash2 simulated fpr
-    plot.plot(range(2, k_values + 1), ideal_rate[1:], marker="x", label=f"c_ideal={c}")         # plots ideal fpr
+    ideal_rate = [(1 - math.exp(-k/c))**k for k in range(1, k_values + 1)]                          # ideal false positive rate formula
 
-    # Find min indices and values
-    hash1_min_idx = hash1_fprs[1:].index(min(hash1_fprs[1:])) + 2                               # +3 to match x-axis (k=3 start)
-    hash1_min_val = min(hash1_fprs[1:])
+    # plotting figure, start from 2 because at 1 the fpr jumps very high    
+    plot.plot(range(2, k_values + 1), hash1_fprs[1:], marker="x", label=f"c={c}, hash1")            # plots hash1 simulated fpr
+    plot.plot(range(2, k_values + 1), hash2_fprs[1:], marker="x", label=f"c={c}, hash2")            # plots hash2 simulated fpr
+    plot.plot(range(2, k_values + 1), ideal_rate[1:], marker="x", label=f"c_ideal={c}")             # plots ideal fpr
 
-    hash2_min_idx = hash2_fprs[1:].index(min(hash2_fprs[1:])) + 2
+    hash1_min_idx = hash1_fprs[1:].index(min(hash1_fprs[1:])) + 2                                   # find min indices and values
+    hash1_min_val = min(hash1_fprs[1:])                                                             # +2 is to match x-axis (k=2 start)
+
+    hash2_min_idx = hash2_fprs[1:].index(min(hash2_fprs[1:])) + 2                                   # repeat for hash2
     hash2_min_val = min(hash2_fprs[1:])
 
-    ideal_min_idx = ideal_rate[1:].index(min(ideal_rate[1:])) + 2
+    ideal_min_idx = ideal_rate[1:].index(min(ideal_rate[1:])) + 2                                   # repeat for ideal              
     ideal_min_val = min(ideal_rate[1:])
 
-    # Mark the minima
-    plot.scatter(hash1_min_idx, hash1_min_val, color='blue', s=75, zorder=5, label='min hash1')
+    plot.scatter(hash1_min_idx, hash1_min_val, color='blue', s=75, zorder=5, label='min hash1')     # plot min points
     plot.scatter(hash2_min_idx, hash2_min_val, color='orange', s=75, zorder=5, label='min hash2')
     plot.scatter(ideal_min_idx, ideal_min_val, color='green', s=75, zorder=5, label='min ideal')
 
-    plot.set_xlabel("k (# of Hash Functions)")                                              # x-axis label
-    plot.set_ylabel("False Positve Rates")                                                  # y-axis label
-    plot.set_title(f"False Positive Rate vs k (c={c})")                                     # title
-    plot.grid(True)                                                                         # enable grid lines
+    plot.set_xlabel("k (# of Hash Functions)")                                                      # x-axis label
+    plot.set_ylabel("False Positve Rates")                                                          # y-axis label
+    plot.set_title(f"False Positive Rate vs k (c={c})")                                             # title
+    plot.grid(True)                                                                                 # enable grid lines
 
-'''
-# Parameters for plotting all the k and c value pairings
-k_values = 15                      # Number of hash functions we will test for each hash type and c_value
-n = 1000                           # Size of subset S
-tests = 10                         # Number of tests ran for each pair of k_value and c_value
-c_values = [5, 7, 9, 11]           # Different c values to test
-hash_types = [1, 2]                # Different hash functions, hash1 and hash2 implemented in part 1
 
-# Create subplots: one for each c value, both hash types on same plot
+# parameters for plotting all the k and c value pairings
+k_values = 15                      # number of hash functions we will test for each hash type and c_value
+n = 10000                          # size of subset S
+tests = 10                         # number of tests ran for each pair of k_value and c_value
+c_values = [5, 7, 9, 11]           # different c values to test
+hash_types = [1, 2]                # different hash functions, hash1 and hash2 implemented in part 1
+
+# create subplots: one for each c value, both hash types on same plot
 fig, axes = plt.subplots(2, 2, figsize=(14, 5))
 axes = axes.ravel()
 (plot1, plot2, plot3, plot4) = axes
 
-for c in c_values:
+for c in c_values:                 # run plot_fpr for each c value
     print(f"Plot for c={c}")
     if c == 5:
         plot = plot1
@@ -251,13 +241,13 @@ for c in c_values:
         plot = plot4
     plot_fpr(n, N, c, k_values, tests, plot)
 
-# Add legends to both subplots
+# add legends to both subplots
 plot1.legend()
 plot2.legend()
 plot3.legend()
 plot4.legend()
 
-# Adjust layout and show
+# adjust layout and show
 plt.tight_layout()
 plt.show()
-'''
+
